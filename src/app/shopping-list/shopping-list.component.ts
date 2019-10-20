7102e11e911cac2fc5c5dead57e10f643ca41e38
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-10-20 11:24:42 -0400

    showing ingredients list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Kale Bushels', 2),
+    new Ingredient('Avocados', 5),
+
+  ];
 
   constructor() { }
 
