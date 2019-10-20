5f707af0ced8ad9790f0f8177bfa3cf03fe07098
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-10-20 00:00:46 -0400

    adding recipe details component without the functionality of being able to view recipe details per click of recipe yet

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,12 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('Kale & Avo Salad','Organic salad using the freshest ingredients',
+    'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+    new Recipe('Kale & Avo Salad','Organic salad using the freshest ingredients',
+    'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+    new Recipe('Kale & Avo Salad','Organic salad using the freshest ingredients',
+    'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+    new Recipe('Kale & Avo Salad','Organic salad using the freshest ingredients',
     'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60')
   ];
 
